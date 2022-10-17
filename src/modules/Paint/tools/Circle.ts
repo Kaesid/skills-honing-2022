02025a81a504698b166de378d89993fd807cde48
Kaesid <keskaesid@gmail.com>
2022-10-17 23:15:07 +0400

    feat: change circle algorithm to oval

--- src/modules/Paint/tools/Circle.ts
+++ src/modules/Paint/tools/Circle.ts
@@ -8,17 +8,30 @@ class Circle extends Tool {
 
   handleDraw() {
     this.ctx.fillStyle = this.color.current;
-    const width = this.position.x - this.startPosition.x;
-    const height = this.position.y - this.startPosition.y;
-
+    this.ctx.lineWidth = 5;
     if (this.savedCanvasDataRef.current) this.ctx.putImageData(this.savedCanvasDataRef.current, 0, 0);
 
-    // this.ctx.fillRect(this.startPosition.x, this.startPosition.y, width, height);
-    this.ctx.arc(this.startPosition.x, this.startPosition.y, Math.sqrt(width ** 2 + height ** 2), 0, 3 * Math.PI);
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.startPosition.x, this.startPosition.y + (this.position.y - this.startPosition.y) / 2);
+    this.ctx.bezierCurveTo(
+      this.startPosition.x,
+      this.startPosition.y,
+      this.position.x,
+      this.startPosition.y,
+      this.position.x,
+      this.startPosition.y + (this.position.y - this.startPosition.y) / 2
+    );
+    this.ctx.bezierCurveTo(
+      this.position.x,
+      this.position.y,
+      this.startPosition.x,
+      this.position.y,
+      this.startPosition.x,
+      this.startPosition.y + (this.position.y - this.startPosition.y) / 2
+    );
+    this.ctx.closePath();
     this.ctx.fill();
   }
-
-  handleDrawFinish() {}
 }
 
 export { Circle };
